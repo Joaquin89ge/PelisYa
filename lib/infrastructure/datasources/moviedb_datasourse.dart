9c7585299e520668dd6e652af6f9ac471a5a68b7
joaquin89ge <joaquin89ge@gmail.com>
2025-01-17 23:35:19 -0300

    mapper and datasource finish

--- lib/infrastructure/datasources/moviedb_datasourse.dart
+++ lib/infrastructure/datasources/moviedb_datasourse.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:pelis_ya/config/constants/environment.dart';
 import 'package:pelis_ya/config/domain/datasources/movies_datasources.dart';
+
 import 'package:pelis_ya/config/domain/entities/movie.dart';
+import 'package:pelis_ya/infrastructure/mappers/movie_mapper.dart';
+import 'package:pelis_ya/infrastructure/models/moviedb/moviedb_response.dart';
 
 class MoviedbDatasourse extends MoviesDatasources {
   final dio = Dio(BaseOptions(
@@ -13,8 +16,11 @@ class MoviedbDatasourse extends MoviesDatasources {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get("/movie/now_playing");
-    response.data;
-    final List<Movie> movies = [];
+    final moviedbresponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = moviedbresponse.results
+        .where((moviedb) => moviedb.posterPath != "no-poster")
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
     return movies;
   }
 }
